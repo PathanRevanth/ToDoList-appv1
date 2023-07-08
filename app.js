6aac3bff3f3af93c40582b9739039ab22055f750
PathanRevanth <pathanrevanth48@gmail.com>
2023-07-08 16:28:20 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js");
+
+
+const app = express();
+
+
+const items = ["Buy Food","Cook Food","Eat Food"];
+app.set('view engine','ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+const day = date.getDate();
+res.render("list",{kindOfDay:day,newListItems:items});
+});
+
+
+app.post("/",function(req,res){
+    const item = req.body.newItem;
+    items.push(item);
+res.redirect("/");
+});
+
+app.listen(3000, function(){
+    console.log("server started on port :3000");
+});
